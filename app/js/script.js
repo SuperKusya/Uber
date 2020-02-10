83baffb7f9c14587b6dbe3cfbe3e0f49ffbd9dfb
kusya <tizahodieslichto@gmail.com>
2020-02-11 00:26:33 +0300

    edit script code for support old browsers such as IE11

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+"use strict";
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+window.addEventListener('DOMContentLoaded', function () {
+  var menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu_item'),
+      hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+  });
+  menuItem.forEach(function (item) {
+    item.addEventListener('click', function () {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
     });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+  });
+});
